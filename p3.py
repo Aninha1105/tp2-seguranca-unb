e97853104017d08aca199588b628531362c9c231
ogjoaoc <joaocarlgoncalvesfilho@gmail.com>
2025-06-14 20:19:41 -0300

    add part 3

--- p3.py
+++ p3.py
@@ -6,18 +6,20 @@ from Crypto.Util.Padding import pad
 from Crypto.Random import get_random_bytes
 
 key = get_random_bytes(16)
-txt = b"seila"
+txt = b"ok"
 
 def run_aes(k, data, mode):
 
-    cipher = AES.new(key, mode)
+    # instância da cifra (usa vi e nonce aleatório da biblioteca).
+    cipher = AES.new(k, mode)
 
+    # aplica padding (ECB e CBC exigem, CFB, OFB, CTR não exigem). 
     if mode in [AES.MODE_ECB, AES.MODE_CBC]:
-        to_cipher = pad(data, AES.block_size)
+        pt = pad(data, AES.block_size)
     else:
-        to_cipher = data
+        pt = data
 
-    ct = cipher.encrypt(to_cipher)
+    ct = cipher.encrypt(pt)
 
     iv_nonce = None
     res = None
@@ -31,14 +33,46 @@ def run_aes(k, data, mode):
         iv_nonce = cipher.iv
         res = cipher.iv + ct
     
-    print(res)
     res_b64 = base64.b64encode(res).decode('utf-8')
 
     return {
         "b64": res_b64,
-        "iv_ou_nonce_hex": iv_nonce.hex() if iv_nonce else "N/A"
+        "iv_nonce_hex": iv_nonce.hex() if iv_nonce else "N/A"
     }
 
-# ainda tem q organizar pra printar isso
 
-print(run_aes(key, txt, AES.MODE_ECB))
+print("=" * 70)
+print("SETUP")
+print(f"Chave (Hex): {key.hex().upper()}")
+print(f"Mensagem Original: {txt.decode('utf-8')}")
+print("=" * 70)
+
+time_result = {}
+
+aes_modes = [
+    ("ECB", AES.MODE_ECB),
+    ("CBC", AES.MODE_CBC),
+    ("CFB", AES.MODE_CFB),
+    ("OFB", AES.MODE_OFB),
+    ("CTR", AES.MODE_CTR)
+]
+
+for mode_name, param in aes_modes:
+    print(f"\n--- MODO {mode_name} ---")
+
+    begin = time.perf_counter()
+    res = run_aes(key, txt, param)
+    end = time.perf_counter()
+
+    calc_time = (end-begin)*1000 # ms
+    time_result[mode_name] = calc_time
+    
+    print(f"IV/Nonce (Hex): {res['iv_nonce_hex'].upper()}")
+    print(f"Texto Cifrado (Base64): {res['b64']}")
+    print(f"Tempo de Execução: {calc_time:.6f} ms")
+    print("-" * 70)    
+
+print("\n\n--- DESEMPENHO ---")
+for mode_name, t in sorted(time_result.items(), key=lambda item:item[1]):
+    print(f"Modo {mode_name:<5}: {t:.6f} ms")
+print("=" * 70)
