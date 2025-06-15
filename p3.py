944db02005aa876314141312aa4eefe903bc3adc
Aninha1105 <analuparaguassu01@gmail.com>
2025-06-15 20:27:56 -0300

    perf: switch crypto lib for faster algorithm + minor fixes

--- p3.py
+++ p3.py
@@ -1,42 +1,50 @@
 # Parte 3: AES real com biblioteca
 import time
 import base64
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
-from Crypto.Random import get_random_bytes
+import os
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
 
-key = get_random_bytes(16)
+key = os.urandom(16)
 txt = b"ok"
 
 def run_aes(k, data, mode):
 
-    # instância da cifra (usa vi e nonce aleatório da biblioteca).
-    cipher = AES.new(k, mode)
-
     # aplica padding (ECB e CBC exigem, CFB, OFB, CTR não exigem). 
-    if mode in [AES.MODE_ECB, AES.MODE_CBC]:
-        pt = pad(data, AES.block_size)
+    if mode in ["ECB", "CBC"]:
+        padder = padding.PKCS7(128).padder()
+        pt = padder.update(data) + padder.finalize()
     else:
         pt = data
 
-    ct = cipher.encrypt(pt)
-
     iv_nonce = None
-    res = None
 
-    if mode == AES.MODE_ECB:
-        res = ct
-    elif mode == AES.MODE_CTR:
-        iv_nonce = cipher.nonce
-        res = cipher.nonce + ct
-    else:
-        iv_nonce = cipher.iv
-        res = cipher.iv + ct
+    if mode == "ECB":
+        cipher = Cipher(algorithms.AES(k), modes.ECB(), backend=default_backend())
+        encryptor = cipher.encryptor()
+    elif mode == "CBC":
+        iv_nonce = os.urandom(16)
+        cipher = Cipher(algorithms.AES(k), modes.CBC(iv_nonce), backend=default_backend())
+        encryptor = cipher.encryptor()
+    elif mode == "CFB":
+        iv_nonce = os.urandom(16)
+        cipher = Cipher(algorithms.AES(k), modes.CFB(iv_nonce), backend=default_backend())
+        encryptor = cipher.encryptor()
+    elif mode == "OFB":
+        iv_nonce = os.urandom(16)
+        cipher = Cipher(algorithms.AES(k), modes.OFB(iv_nonce), backend=default_backend())
+        encryptor = cipher.encryptor()
+    elif mode == "CTR":
+        iv_nonce = os.urandom(16)
+        cipher = Cipher(algorithms.AES(k), modes.CTR(iv_nonce), backend=default_backend())
+        encryptor = cipher.encryptor()
     
-    res_b64 = base64.b64encode(res).decode('utf-8')
+    ct = encryptor.update(pt) + encryptor.finalize()
+    res = (iv_nonce + ct) if iv_nonce else ct
 
     return {
-        "b64": res_b64,
+        "b64": base64.b64encode(res).decode('utf-8'),
         "iv_nonce_hex": iv_nonce.hex() if iv_nonce else "N/A"
     }
 
@@ -49,23 +57,17 @@ print("=" * 70)
 
 time_result = {}
 
-aes_modes = [
-    ("ECB", AES.MODE_ECB),
-    ("CBC", AES.MODE_CBC),
-    ("CFB", AES.MODE_CFB),
-    ("OFB", AES.MODE_OFB),
-    ("CTR", AES.MODE_CTR)
-]
+aes_modes = ["ECB", "CBC", "CFB", "OFB", "CTR"]
 
-for mode_name, param in aes_modes:
-    print(f"\n--- MODO {mode_name} ---")
+for mode in aes_modes:
+    print(f"\n--- MODO {mode} ---")
 
     begin = time.perf_counter()
-    res = run_aes(key, txt, param)
+    res = run_aes(key, txt, mode)
     end = time.perf_counter()
 
     calc_time = (end-begin)*1000 # ms
-    time_result[mode_name] = calc_time
+    time_result[mode] = calc_time
     
     print(f"IV/Nonce (Hex): {res['iv_nonce_hex'].upper()}")
     print(f"Texto Cifrado (Base64): {res['b64']}")
